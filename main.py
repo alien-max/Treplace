705b98bcc917843b9fd50e2454f397befeeea26a
pablo <108761196+alien-max@users.noreply.github.com>
2022-10-28 22:11:47 +0330

    main

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 chars = ['!', '@', '#', '$', '%', '^', '&', '*', '/', '\\', '<', '>', '?', '|']
 
 def openFile():
-    tf = filedialog.askopenfilename(initialdir="C:/Users/MainFrame/Desktop/", title="باز کردن فایل متنی", filetypes=(("Text Files", "*.txt"),))
+    tf = filedialog.askopenfilename(initialdir="C:/Users/MainFrame/Desktop/", title="Import text file", filetypes=(("Text Files", "*.txt"),))
     tf = open(tf, encoding='utf-8')
     data = tf.read()
     txtarea.insert(END, data)
@@ -214,7 +214,7 @@ def char_rem():
 
 
 def export():
-    tf = filedialog.asksaveasfilename(initialdir="C:/Users/MainFrame/Desktop/", title="ذخیره خروجی", filetypes=(("Text files", "*.txt"), ))
+    tf = filedialog.asksaveasfilename(initialdir="C:/Users/MainFrame/Desktop/", title="Export text file", filetypes=(("Text files", "*.txt"), ))
     file_name = tf + '.txt'
     res_file = open(file_name, 'w', encoding="utf-8")
     res_file.write(resarea.get("1.0","end"))
@@ -232,34 +232,33 @@ def reset_fields():
 def callback(url):
    webbrowser.open_new_tab(url)
 
+txtarea = scrolledtext.ScrolledText(root, font=("Vazir", 10))
+txtarea.place(x=10, y=30, width=380, height=370)
+Label(root, text="Input", font=("Vazir", 11, 'bold'), fg="#fff", bg='#243665').place(x=175, y=5, width=50, height=25)
 
 resarea = scrolledtext.ScrolledText(root, font=("Vazir", 10))
-resarea.place(x=10, y=10, width=380, height=390)
+resarea.place(x=410, y=30, width=380, height=370)
+Label(root, text="Output", font=("Vazir", 11, 'bold'), fg="#fff", bg='#243665').place(x=575, y=5, width=50, height=25)
 
-txtarea = scrolledtext.ScrolledText(root, font=("Vazir", 10))
-txtarea.place(x=410, y=10, width=380, height=390)
+Button(root, text="Import", font=("Vazir", 10), command=openFile).place(x=110, y=420, width=180, height=30)
+Button(root, text="Reset", font=("Vazir", 10), command=reset_fields).place(x=375, y=405, width=50, height=30)
+Button(root, text="Export", font=("Vazir", 10), command=export).place(x=510, y=420, width=180, height=30)
 
-Button(root, text="دریافت خروجی", font=("Vazir", 10), command=export).place(x=110, y=420, width=180, height=30)
-Button(root, text="ریست", font=("Vazir", 10), command=reset_fields).place(x=375, y=405, width=50, height=30)
-Button(root, text="وارد کردن فایل", font=("Vazir", 10), command=openFile).place(x=510, y=420, width=180, height=30)
-
-
-Button(root, text="حذف کلمات انگلیسی", font=("Vazir", 10), command=eng_rem).place(x=110, y=460, width=180, height=30)
-Button(root, text="حذف کاراکتر ها", font=("Vazir", 10), command=char_rem).place(x=310, y=460, width=180, height=30)
-Button(root, text="حذف اعداد", font=("Vazir", 10), command=num_rem).place(x=510, y=460, width=180, height=30)
-
-
-rep_new = Entry(root, font=("Vazir", 10))
-rep_new.place(x=110, y=500, width=180, height=30)
-Label(root, text="به (با فاصله جدا شود)", font=("Vazir", 8), fg="#fff", bg='#243665').place(x=110, y=540, width=180, height=20)
-
-Button(root, text="تبدیل", font=("Vazir", 10), command=import_urep).place(x=310, y=500, width=180, height=30)
+Button(root, text="Remove Words", font=("Vazir", 10), command=eng_rem).place(x=110, y=460, width=180, height=30)
+Button(root, text="Remove Chars", font=("Vazir", 10), command=char_rem).place(x=310, y=460, width=180, height=30)
+Button(root, text="Remove Nums", font=("Vazir", 10), command=num_rem).place(x=510, y=460, width=180, height=30)
 
 rep_old = Entry(root, font=("Vazir", 10))
-rep_old.place(x=510, y=500, width=180, height=30)
-Label(root, text="از (با فاصله جدا شود)", font=("Vazir", 8), fg="#fff", bg='#243665').place(x=510, y=540, width=180, height=20)
+rep_old.place(x=110, y=500, width=180, height=30)
+Label(root, text="from(Separate with space)", font=("Vazir", 8), fg="#fff", bg='#243665').place(x=110, y=535, width=180, height=20)
 
-link = Label(root, text="Alien Max", font=("Vazir", 8), bg='#243665', cursor="hand2")
+Button(root, text="Convert", font=("Vazir", 10), command=import_urep).place(x=310, y=500, width=180, height=30)
+
+rep_new = Entry(root, font=("Vazir", 10))
+rep_new.place(x=510, y=500, width=180, height=30)
+Label(root, text="to(Separate with space)", font=("Vazir", 8), fg="#fff", bg='#243665').place(x=510, y=535, width=180, height=20)
+
+link = Label(root, text="Alien Max", font=("Vazir", 8, 'bold'), bg='#243665', cursor="hand2")
 link.place(x=370, y=575)
 link.bind("<Button-1>", lambda e: callback("https://github.com/alien-max"))
 
